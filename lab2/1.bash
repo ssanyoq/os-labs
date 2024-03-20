c9453d9b1075a7213d765f330b852b7fc216a0c8
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2024-03-20 17:13:23 +0300

    Insanely good first

--- lab2/1.bash
+++ lab2/1.bash
@@ -1,6 +1,12 @@
-#! /bin/bash
+#!/bin/bash
 
-let amt=$(ps -u user | wc -l)-3 #bash, script, ps
-echo $amt > first.txt
-#ps -u user
-ps -u user -o pid,cmd | head -n -4
+# Get the PID of the current script
+SCRIPT_PID=$$
+
+# Get the PPIDs of processes spawned by the script
+CHILD_PPIDS=$(pgrep -P $SCRIPT_PID)
+echo $SCRIPT_PID
+echo $CHILD_PPIDS
+# Print out ps excluding processes with the script's PID and its child PPIDs
+ps -eo pid,ppid,cmd | grep -v "\<$$\>" | awk '{print $1, $3}' 
+
