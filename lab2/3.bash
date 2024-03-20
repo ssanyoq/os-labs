a362c3198e7ead63267f009058edb96a9818e614
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2024-03-20 23:37:13 +0300

    Fully working bash-ignoring third

--- lab2/3.bash
+++ lab2/3.bash
@@ -1,4 +1,5 @@
 #! /bin/bash
 
-ps -e -o pid,lstart,cmd --sort=start_time | tac | awk 'NR==3 {print $1, $2, $3, $4, $5, $6}'
-#3rd last, before ps -e .. process and ./3.bash
+
+ps -eo pid,ppid,stime,cmd --sort=-stime | grep -E -v "\<$$\>" | head -n 2 | awk '{print $1,$3}'
+
