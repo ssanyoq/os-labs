093133c24685645323585a70ba6e5368410de132
sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2024-03-22 01:26:22 +0300

    Working??

--- lab2/7.bash
+++ lab2/7.bash
@@ -1,8 +1,7 @@
 #! /bin/bash
 
-DELAY=6
+DELAY=60
 
->tmp/pids.txt
 
 #if [[ -v array[a] ]] then echo 'aaaaaaa'
 
@@ -12,7 +11,7 @@ declare -A retro
 
 for pid_dir in /proc/[0-9]*; do 
 	pid=$(basename $pid_dir)
-	if [[ -d $pid_dir && $pid =~ ^[0-9]+$ && $pid -ne $$ ]];
+	if [[ -d $pid_dir && $pid =~ ^[0-9]+$ && -v $pid_dir/io ]];
 	then
 		read_bytes=$(cat $pid_dir/io | grep "read_bytes" | awk '{print $2}')
 		retro[$pid]=$read_bytes
@@ -29,16 +28,32 @@ end_time=$(( $(date +%s) + $DELAY ))
 while [ $(date +%s) -lt $end_time ]; do 
 	cur_pids=$(pgrep -d ' ' .)
 	for pid in $cur_pids; do
-		read_bytes=$(grep "read_bytes" "/proc/$pid/io" | awk '{print $2}')
-		new[$pid]=$read_bytes
+		if [[ -d /proc/$pid ]] ;then
+			read_bytes=$(grep "read_bytes" "/proc/$pid/io" | awk '{print $2}')
+			new[$pid]=$read_bytes
+		fi
 	done
     	sleep 1
 done
 
-for i in "${!new[@]}"
+#for i in "${!new[@]}"
+#do
+#echo "${i}=${new[$i]}"
+#done
+
+>tmp/data.txt
+
+for pid in "${!new[@]}"
 do
-echo "${i}=${new[$i]}"
+	if [[ new[$pid] -eq 0 ]] then
+		continue
+	fi
+	data_taken=${new[$pid]}
+	if [[ -v retro[$pid] ]] then
+		let data_taken=$data_taken-${retro[$pid]}
+	fi
+	cmd=$(cat /proc/$pid/cmdline | tr '\0' ' ')
+	echo "$pid : $cmd: $data_taken" >> tmp/data.txt
 done
 
-
-
+sort -t ':' -k3n tmp/data.txt | tac | head -n 3
