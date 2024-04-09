bcc184f5330af495fe9e251d60ae5135d0ce5787
sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2024-04-10 01:38:28 +0300

    feat: complete 1.bash, add Makefile target for cleaning

--- lab3/1.bash
+++ lab3/1.bash
@@ -1,9 +1,9 @@
 #! /bin/bash
-
+datetime=$(date +"%b_%d_%H:%M")
 mkdir test && {
 	echo "catalog test was created successfully" > report.txt
-	touch test/"aa" 
+	touch test/$datetime
 }
 
-ping net_nikogo.ru || echo "Na net nikogo nikogo net" >> report.txt
+ping net_nikogo.ru || echo "$datetime: Na net nikogo nikogo net" >> report.txt
 
