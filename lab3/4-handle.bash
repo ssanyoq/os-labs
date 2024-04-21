06582c06f042344b657b263587fdadcbe0e75206
sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2024-04-21 14:46:45 +0300

    fix: Fix CPU% limiter

--- lab3/4-handle.bash
+++ lab3/4-handle.bash
@@ -4,8 +4,16 @@
 ./4-gen.bash&PID2=$!
 ./4-gen.bash&PID3=$!
 
-#echo $PID1 $PID2 $PID3
-cpulimit --pid $PID1 --limit 10 -b
+echo $PID1 $PID2 $PID3
+while true; do
+	(cpu=$(ps -p $PID1 -o %cpu | awk '{ print $1 }' | tail -1)
+	if [[ $(echo "$cpu > 10" | bc -l) -eq 1 ]] then
+		renice -n 19 $PID1 2> /dev/null
+		sleep 2
+		renice -n 0 $PID1 2> /dev/null
+	fi)
+done
+cpulimit --pid $PID1 --limit 10 &
 kill $PID3
 
 
