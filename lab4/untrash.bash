5fc83bc965865bb451f0098d493adfdd09ac8814
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2024-05-01 22:42:44 +0300

    feat: Complete untrashing command

--- lab4/untrash.bash
+++ lab4/untrash.bash
@@ -19,11 +19,49 @@ fi
 
 #-------------------------------------------------------
 
-
-while IFS=" " read -r quotes_filepath link; do 
+matches=0
+>tmp.log
+while IFS= read -r line; do
+  keep=1
+  quotes_filepath=$(awk '{ last = $NF; $NF = ""; print substr($0, 1, length($0)-1) }' <<< $line)
+  link=$(awk '{ print $NF }' <<< $line)
   filepath=${quotes_filepath:1:-1}
-  filename=$(basename $filepath)
+  filename=$(awk -F '/' '{ print $NF }' <<< $filepath)
+  # echo path: $filepath
+  # echo file: $filename
+  # echo arg: $1
+  echo ""
   if [[ $filename == $1 ]]; then
-    echo "match found"
+    ((matches++))
+    echo "Match found: $filepath"
+    echo "" 
+    read -p "Do you want to untrash it? [Y/n] " choice </dev/tty
+    case "$choice" in
+      [Yy]*) 
+        if [[ -e $filepath ]]; then
+          number=0
+          while [[ -e $filepath\_$number ]]; do
+            ((number++))
+          done
+          filepath=$filepath\_$number
+        fi  
+        ln "trash/$link" "$filepath"
+        echo "File untrashed successfully"
+        keep=0
+        ;;
+      *) echo Skipping file
+      ;;
+    esac
+  fi
+  if [[ $keep -eq 1 ]]; then
+    echo $line >> tmp.log
   fi
 done < $LOG_FILE
+
+rm $LOG_FILE
+mv tmp.log $LOG_FILE
+
+if [[ matches -eq 0 ]]; then
+  echo "No matches found"
+fi
+
