afcb9c4e398b0f1cb8f231cca0e03e339a1642a6
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2024-05-01 22:42:20 +0300

    feat: Add backing up feature in backup, not finished

---
+++ lab4/backup.bash
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+
+BACKUP_DIR=$HOME
+SOURCE_DIR=$HOME/source
+let MIN_ALLOWED_DIFF_S=7*24*60*60
+
+cur_date=$(date +"%Y-%m-%d")
+backup_path=$BACKUP_DIR/"Backup-$cur_date"
+
+# Checking if there is recent enough backup
+
+max_s=0
+
+for name in $(find $BACKUP_DIR -type d -name "Backup-*")
+do
+  cut_date=$(echo $name | awk -F '-' '{ print $2"-"$3"-"$4 }')
+  s=$(date -d $cut_date +%s)
+  if [[ $s -gt $max_s ]]; then
+    max_s=$s
+  fi
+done
+
+
+let diff=($(date +%s) - $max_s)
+
+if [[ $diff -lt $MIN_ALLOWED_DIFF_S ]]; then
+  echo "Relevant version of backup already exists."
+  exit 1
+fi
+
+mkdir $backup_path
+
+cp -R $SOURCE_DIR/* $backup_path
+
+
