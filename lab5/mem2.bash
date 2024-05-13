b661841fa05ce4f6c47f2ae3558031b0c0e5a322
root <capsynice@gmail.com>
2024-05-13 15:17:37 -0400

    feat: Add slightly different mem.bash

---
+++ lab5/mem2.bash
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+LOG_STEP=100000
+LOG_FILE=report2.log 
+>$LOG_FILE
+
+array=(0)
+
+i=0
+
+while [[ 1 ]]; do
+  for ((j = $i; j < i+10; j++)); do
+    array+=($j)
+  done
+  let i=$i+10
+  if [[ $(($i % $LOG_STEP)) -eq 0 ]]; then
+    echo ${#array[*]} >> $LOG_FILE
+    # echo "+1"
+  fi
+done
+
+
