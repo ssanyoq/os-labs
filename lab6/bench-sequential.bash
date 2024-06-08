e8fac9c64993c082a9b3e9d8a4f614611e41bcfc
root <capsynice@gmail.com>
2024-06-08 17:54:03 -0400

    feat: Mute output

--- lab6/bench-sequential.bash
+++ lab6/bench-sequential.bash
@@ -8,7 +8,7 @@ for ((i = 1; i <= 20; i++)); do
     time=$( (time ./sequential-launch.bash $i > /dev/null 2>&1) 2>&1 )
     
     real_time=$(echo $time | grep "real" | awk '{print $2}')
-    echo $real_time
+    # echo $real_time
     echo "$i $real_time" >> data/sequential.log
   done
   
