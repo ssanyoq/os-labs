20566e440f3a0bedaab48cd68f55490e49baba91
Aleksandr Kutsalo <60144890+Ssanyoq@users.noreply.github.com>
2024-06-20 14:31:33 +0300

    fix: Fix typo in gen.bash

--- lab6/gen.bash
+++ lab6/gen.bash
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Script creates a new text file of name that is specified as an argument
-# It fills is with random numbers(as needed in the lab description)
+# It fills it with random numbers(as needed in the lab description)
 # Resulting file with LIM=2000000 takes up around 11MB of disk space
 
 if [[ -z $1 ]]; then
